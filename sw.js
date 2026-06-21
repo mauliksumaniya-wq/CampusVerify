88c5a75c35a4a4058115a839eab4ffef2b9fded7
mauliksumaniya-wq <mauliksumaniya@gmail.com>
2026-06-21 21:19:37 +0530

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
 const CACHE_NAME = 'campusverify-v1';
 const urlsToCache = ['./'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
